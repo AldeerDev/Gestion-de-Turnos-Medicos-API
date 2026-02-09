f6d78ba2e322ee29006434fcf2faefaee6bd6efb
AldeerDev <aldeerdev@gmail.com>
2026-02-09 11:22:41 -0300

    Agrega endpoint POST /medicos crea medicos

--- src/main/java/com/aldeerdev/apiturnosmedicos/controller/MedicoController.java
+++ src/main/java/com/aldeerdev/apiturnosmedicos/controller/MedicoController.java
@@ -1,8 +1,22 @@
 package com.aldeerdev.apiturnosmedicos.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.aldeerdev.apiturnosmedicos.model.Medico;
+import com.aldeerdev.apiturnosmedicos.service.MedicoService;
 
 @RestController
 public class MedicoController {
+
+	@Autowired
+	private MedicoService service;
+
+	@PostMapping("/medicos")
+	public Medico crearMedico(@RequestBody Medico medico) {
+		return service.crearMedico(medico);
+	}
 
 }
--- src/main/java/com/aldeerdev/apiturnosmedicos/service/MedicoService.java
+++ src/main/java/com/aldeerdev/apiturnosmedicos/service/MedicoService.java
@@ -1,8 +1,18 @@
 package com.aldeerdev.apiturnosmedicos.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.aldeerdev.apiturnosmedicos.model.Medico;
+import com.aldeerdev.apiturnosmedicos.repository.MedicoRepository;
 
 @Service
 public class MedicoService {
 
+	@Autowired
+	private MedicoRepository repository;
+
+	public Medico crearMedico(Medico medico) {
+		return repository.save(medico);
+	}
 }
