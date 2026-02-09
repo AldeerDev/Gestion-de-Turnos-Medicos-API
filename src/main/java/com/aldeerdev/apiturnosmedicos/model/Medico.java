eff6dfe27ee1339a6f3b2307cda1f7c23df6b8c3
AldeerDev <aldeerdev@gmail.com>
2026-02-09 09:53:05 -0300

    Agrega entidad Medico

---
+++ src/main/java/com/aldeerdev/apiturnosmedicos/model/Medico.java
@@ -0,0 +1,75 @@
+package com.aldeerdev.apiturnosmedicos.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "medicos")
+public class Medico {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nombre;
+	private String apellido;
+	private String especialidad;
+	private String matricula;
+	private boolean activo;
+
+	public Medico() {
+		super();
+	}
+
+	public Medico(String nombre, String apellido, String especialidad, String matricula, boolean activo) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.especialidad = especialidad;
+		this.matricula = matricula;
+		this.activo = activo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEspecialidad() {
+		return especialidad;
+	}
+
+	public void setEspecialidad(String especialidad) {
+		this.especialidad = especialidad;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public boolean isActivo() {
+		return activo;
+	}
+
+	public void setActivo(boolean activo) {
+		this.activo = activo;
+	}
+
+}
