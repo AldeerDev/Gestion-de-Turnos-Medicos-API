69c9742ddcb3f72c1824f258f6dacc956769767e
AldeerDev <aldeerdev@gmail.com>
2026-02-09 20:49:44 -0300

    refactor: lanzar una excepcion cuando paciente no se encuentra en el servicio

--- src/main/java/com/aldeerdev/apiturnosmedicos/service/PacienteService.java
+++ src/main/java/com/aldeerdev/apiturnosmedicos/service/PacienteService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.aldeerdev.apiturnosmedicos.exception.PacienteNotFoundException;
 import com.aldeerdev.apiturnosmedicos.model.Paciente;
 import com.aldeerdev.apiturnosmedicos.repository.PacienteRepository;
 
@@ -17,15 +18,15 @@ public class PacienteService {
 	public Paciente crearPaciente(Paciente paciente) {
 		return repository.save(paciente);
 	}
-	
+
 	public Paciente obtenerPacientePorId(Long id) {
-		return repository.findById(id).orElse(null);
+		return repository.findById(id).orElseThrow(() -> new PacienteNotFoundException(id));
 	}
 
 	public List<Paciente> listarPacientes() {
 		return repository.findAll();
 	}
-	
+
 	public Paciente actualizarPaciente(Long id, Paciente paciente) {
 		Paciente actualizado = repository.findById(id).orElse(null);
 		actualizado.setNombre(paciente.getNombre());
@@ -35,7 +36,7 @@ public class PacienteService {
 		actualizado.setFechaNacimiento(paciente.getFechaNacimiento());
 		return repository.save(actualizado);
 	}
-	
+
 	public void eliminarPaciente(Long id) {
 		Paciente pacienteBaja = repository.findById(id).orElse(null);
 		pacienteBaja.setActivo(false);
