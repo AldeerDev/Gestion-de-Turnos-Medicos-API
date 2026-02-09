8a18e047cbd24012e7fea06f470527d0baf70f50
AldeerDev <aldeerdev@gmail.com>
2026-02-09 12:20:15 -0300

    Agrega endpoint GET/medicos/{especialidad} devuelve medicos de la especialidad

--- src/main/java/com/aldeerdev/apiturnosmedicos/controller/MedicoController.java
+++ src/main/java/com/aldeerdev/apiturnosmedicos/controller/MedicoController.java
@@ -1,6 +1,10 @@
 package com.aldeerdev.apiturnosmedicos.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +17,16 @@ public class MedicoController {
 
 	@Autowired
 	private MedicoService service;
+	
+	@GetMapping("/medicos/{especialidad}")
+	public List<Medico> listarMedicosPorEspecialidad(@PathVariable String especialidad) {
+		return service.listarMedicosPorEspecialidad(especialidad);
+	}
 
 	@PostMapping("/medicos")
 	public Medico crearMedico(@RequestBody Medico medico) {
 		return service.crearMedico(medico);
 	}
+	
 
 }
--- src/main/java/com/aldeerdev/apiturnosmedicos/service/MedicoService.java
+++ src/main/java/com/aldeerdev/apiturnosmedicos/service/MedicoService.java
@@ -1,5 +1,7 @@
 package com.aldeerdev.apiturnosmedicos.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +16,9 @@ public class MedicoService {
 
 	public Medico crearMedico(Medico medico) {
 		return repository.save(medico);
+	}
+
+	public List<Medico> listarMedicosPorEspecialidad(String especialidad) {
+		return repository.findAll().stream().filter(m -> m.getEspecialidad().equals(especialidad)).toList();
 	}
 }
