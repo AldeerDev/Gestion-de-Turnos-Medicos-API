a2472166e7a806c05cbd07184e9132aab19ab06d
AldeerDev <aldeerdev@gmail.com>
2026-02-12 21:22:29 -0300

    feat: aplicar la validacion activa de medico en la capa de servicio

--- src/main/java/com/aldeerdev/apiturnosmedicos/service/MedicoService.java
+++ src/main/java/com/aldeerdev/apiturnosmedicos/service/MedicoService.java
@@ -1,10 +1,12 @@
 package com.aldeerdev.apiturnosmedicos.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.aldeerdev.apiturnosmedicos.exception.MedicoInactivoException;
 import com.aldeerdev.apiturnosmedicos.exception.MedicoNotFoundException;
 import com.aldeerdev.apiturnosmedicos.model.Medico;
 import com.aldeerdev.apiturnosmedicos.repository.MedicoRepository;
@@ -22,12 +24,17 @@ public class MedicoService {
 	public List<Medico> listarMedicosPorEspecialidad(String especialidad) {
 		return repository.findAll().stream().filter(m -> m.getEspecialidad().equals(especialidad)).toList();
 	}
-	
+
 	public Medico obtenerMedicoPorId(Long id) {
-		return repository.findById(id).orElseThrow(() -> new MedicoNotFoundException(id));
+		return repository.findById(id).map(medico -> {
+			if (!medico.isActivo()) {
+				throw new MedicoInactivoException();
+			}
+			return medico;
+		}).orElseThrow(() -> new MedicoNotFoundException(id));
 	}
-	
-	public Medico actualizarMedico(Long id,Medico medico) {
+
+	public Medico actualizarMedico(Long id, Medico medico) {
 		Medico actualizado = obtenerMedicoPorId(id);
 		actualizado.setNombre(medico.getNombre());
 		actualizado.setApellido(medico.getApellido());
@@ -35,7 +42,7 @@ public class MedicoService {
 		actualizado.setMatricula(medico.getMatricula());
 		return repository.save(actualizado);
 	}
-	
+
 	public void eliminarMedico(Long id) {
 		Medico bajaMedico = repository.findById(id).orElse(null);
 		bajaMedico.setActivo(false);
